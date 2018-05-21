eccb576c075805e0f93bb60b19013fec5283d9dd
Patryk Rzucidło <ptkdev@gmail.com>
2018-05-22 00:34:45 +0200

    [Feature] Uppercase var and Ignore js func

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
 function isClassUsage(node) {
-  return ['FunctionDeclaration', 'NewExpression', 'MemberExpression'].indexOf(node.parent.type) > -1;
+    return ['FunctionDeclaration', 'NewExpression', 'MemberExpression'].indexOf(node.parent.type) > -1;
+}
+
+function contains(a, obj) {
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] === obj) {
+            return true;
+        }
+    }
+    return false;
 }
 
 module.exports = {
-  rules: {
-    snakecase: {
-      meta: {
-        fixable: true,
-      },
-      create(context) {
-        return {
-          Identifier(node) {
-            var name = node.name;
-            var split = name.split(/(?=[A-Z])/);
-            if (split.length > 1) {
-              if (isClassUsage(node)) {
-                return true;
-              }
-              // this has some uppercase letters
-              context.report({
-                message: 'Identifiers must be snake case: {{ identifier }}',
-                node: node,
-                data: {
-                  identifier: node.name,
-                },
-                // fix(fixer) {
-                //   return fixer.replaceText(node, split.map(function(piece){ return piece.replace('_', '').toLowerCase(); }).join('_'));
-                // }
-              })
+    rules: {
+        snakecase: {
+            meta: {
+                fixable: true,
+            },
+            create(context) {
+                return {
+                    Identifier(node) {
+                        var name = node.name;
+                        var array_system_var = ["parseInt", "parseFloat", "isNaN", "isFinite", "decodeURI", "decodeURIComponent", "encodeURI", "toString", "toLocaleString", "valueOf", "hasOwnProperty", "isPrototypeOf", "propertyIsEnumerable", "indexOf", "forEach", "charAt", "charCodeAt", "endsWith", "lastIndexOf", "startsWith", "toLowerCase", "toLocaleLowerCase", "toUpperCase", "toLocaleUpperCase", "toFixed", "toPrecision", "setInterval", "clearInterval", "setTimeout", "toDateString", "toTimeString", "getTime", "getFullYear", "getUTCFullYear", "getMonth", "getUTCMonth", "getDate", "getUTCDate", "getDay", "getUTCDay", "getHours", "getUTCHours", "getMinutes", "getUTCMinutes", "getSeconds", "getUTCSeconds", "getMilliseconds", "getUTCMilliseconds", "getTimeZoneOffset", "setTime", "setMilliseconds", "setUTCMilliseconds", "setSeconds", "setUTCSeconds", "setMinutes", "setUTCMinutes", "setHours", "setUTCHours", "setDate", "setUTCDate", "setMonth", "setUTCMonth", "setFullYear", "setUTCFullYear", "toUTCString", "toISOString", "toJSON"];
+                        var split = name.split(/(?=[A-Z])/);
+                        var split_az = name.split(/(?=[a-z])/);
+                        if ((split.length > 1 && split_az.length > 1) && !contains(array_system_var, name)) {
+                            if (isClassUsage(node)) {
+                                return true;
+                            }
+                            // this has some uppercase letters
+                            context.report({
+                                message: 'Identifiers must be snake case: {{ identifier }}',
+                                node: node,
+                                data: {
+                                    identifier: node.name,
+                                },
+                                // fix(fixer) {
+                                //   return fixer.replaceText(node, split.map(function(piece){ return piece.replace('_', '').toLowerCase(); }).join('_'));
+                                // }
+                            })
+                        }
+                    }
+                }
             }
-          }
         }
-      }
     }
-  }
 }
