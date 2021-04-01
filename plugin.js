b1fa9cbe3514cfa0203becb873854eb7f6acffcc
ellfs <ellfs.hk@protonmail.com>
2021-04-01 10:30:03 +0800

    refactored and added settings * moved array_system_var into file * added settings: allow_uppercamel, allowed_patterns, allowed_uppercases

--- plugin.js
+++ plugin.js
@@ -11,10 +11,28 @@
  *
  */
 
+// ignore javascript language default function
+const array_system_var = require('./array_system_var');
+// General Camel case pattern
+const general_camelcase_pattern = /^[A-Za-z][a-z0-9]+((\d)|([A-Z0-9][a-z0-9]+))+$/;
+// UpperCamel pattern
+const uppercamelcase_pattern = /^([A-Z][a-z0-9]+)((\d)|([A-Z0-9][a-z0-9]+))+$/;
+
 // Default list of names allowed to be camel cased
 const default_whitelist = [];
 // Default list of parent node types, that allows node to be camel cased
 const default_filter = ["ClassDeclaration", "NewExpression"];
+// Default list of allowed patterns to be camel cased
+const default_allowed_patterns = [];
+// Default list of allowed patterns to be upper cased
+const default_allowed_uppercases = [];
+
+const whitelist_uppercase = (allowed_uppercases, str) => (
+  str.replace(new RegExp(allowed_uppercases.join('|'), 'i'), (match) => match.toUpperCase())
+);
+const to_snakecase = (str) => (
+  str.replace(/(?<=[a-zA-Z])[A-Z]/g, (letter) => `_${letter.toLowerCase()}`)
+);
 
 /**
  * Detect function and variables name
@@ -29,10 +47,18 @@ module.exports = {
 				fixable: true,
 			},
 			create (context) {
-				const raw_whitelist = context.settings["snakecasejs/whitelist"];
-				const whitelist = Array.isArray(context.settings["snakecasejs/whitelist"]) ? raw_whitelist : default_whitelist;
-				const raw_settings = context.settings["snakecasejs/filter"];
-				const settings = Array.isArray(context.settings["snakecasejs/filter"]) ? raw_settings : default_filter;
+				const get_array_field = (key, default_value) => (
+					Array.isArray(context.settings[key])
+						? context.settings[key]
+						: default_value
+				);
+				const whitelist = get_array_field("snakecasejs/whitelist", default_whitelist);
+				const settings = get_array_field("snakecasejs/filter", default_filter);
+				const allow_uppercamel = Boolean(context.settings["snakecasejs/allow_uppercamel"]);
+
+				const allowed_patterns = get_array_field("snakecasejs/allowed_patterns", default_allowed_patterns);
+				const allowed_pattern = new RegExp(allowed_patterns.join('|'));
+				const allowed_uppercases = get_array_field("snakecasejs/allowed_uppercases", default_allowed_uppercases);
 
 				/**
 				 * Indicates if current node should be ignored for error report
@@ -46,31 +72,47 @@ module.exports = {
 
 				return {
 					Identifier (node) {
-						var name = node.name;
-
-						// ignore javascript language default function
-						var array_system_var = ["Array","RegExp","Date","Number","String","Boolean","Error","Global","JSON","Math","Classes","Event","accessKey","activeElement","addEventListener","adoptNode","altKey","animationName","appCodeName","appendChild","appName","appVersion","availHeight","availWidth","baseURI","changeTouches","characterSet","charAt","charCode","charCodeAt","childElementCount","childNodes","classList","className","clearInterval","clearTimeout","clearWatch","clientHeight","clientLeft","clientTop","clientWidth","clientX","clientY","clipboardData","colorDepth","compareDocumentPosition","contentEditable","Statements","cookieEnabled","copyWithin","createAttribute","createComment","createDocumentFragment","createElement","createEvent","createTextNode","ctrlKey","currentTarget","dataTransfer","decodeURI","decodeURIComponent","defaultPrevented","defaultStatus","defaultView","deltaX","deltaY","deltaZ","deltaMode","designMode","documentElement","documentMode","documentURI","domConfig","elapsedTime","encodeURI","encodeURIComponent","endsWith","eventPhase","execCommand","exitFullscreen","findIndex","forEach","frameElement","fromCharCode","fullscreenElement","fullscreenEnabled","getDate","getDay","getAttribute","getAttributeNode","getBoundingClientRect","getComputedStyle","getCurrentPosition","getElementById","getElementsByClassName","getElementsByName","getElementsByTagName","getFullYear","getHours","getItem","getMilliseconds","getMinutes","getModifierState","getMonth","getNamedItem","getSeconds","getSelection","getTargetRanges","getTime","getTimezoneOffset","getUTCDate","getUTCDay","getUTCFullYear","getUTCHours","getUTCMilliseconds","getUTCMinutes","getUTCMonth","getUTCSeconds","getYear","groupCollapsed","groupEnd","hasAttribute","hasAttributes","hasChildNodes","hasFocus","ignoreCase","importNode","indexOf","Infinity","innerHeight","innerHTML","innerText","innerWidth","inputEncoding","inputType","insertAdjacentElement","insertAdjacentHTML","insertAdjacentText","insertBefore","isArray","isComposing","isContentEditable","isDefaultNamespace","isEqualNode","isFinite","isId","isInteger","isNaN","isSafeInteger","isSameNode","isSupported","isTrusted","keyCode","lastChild","lastElementChild","lastIndex","lastIndexOf","lastModified","lengthComputable","LN2","LN10","localeCompare","localStorage","LOG2E","LOG10E","matchMedia","MAX_VALUE","metaKey","MIN_VALUE","moveBy","moveTo","MovementX","MovementY","namedItem","namespaceURI","NaN","NEGATIVE_INFINITY","newURL","newValue","nextSibling","nextElementSibling","nodeName","nodeType","nodeValue","normalizeDocument","Number","offsetHeight","offsetWidth","offsetLeft","offsetParent","offsetTop","offsetX","offsetY","oldURL","oldValue","outerHeight","outerHTML","outerText","outerWidth","ownerDocument","pageX","pageXOffset","pageY","pageYOffset","parentNode","parentElement","parseFloat","parseInt","PI","pixelDepth","positionError","positionOptions","POSITIVE_INFINITY","preventDefault","propertyName","pseudoElement","querySelector","querySelectorAll","readyState","reduceRight","removeAttribute","removeAttributeNode","removeChild","removeEventListener","removeItem","replaceChild","requestAnimationFrame","requestFullscreen","resizeBy","resizeTo","previousSibling","previousElementSibling","prototype","relatedTarget","removeEventListener","removeNamedItem","renameNode","screenLeft","screenTop","screenX","screenY","scrollBy","scrollHeight","scrollIntoView","scrollLeft","scrollTo","scrollTop","scrollWidth","scrollX","scrollY","sessionStorage","setAttribute","setAttributeNode","setDate","setFullYear","setHours","setInterval","setItem","setMilliseconds","setMinutes","setMonth","setNamedItem","setSeconds","setTime","setTimeout","setUTCDate","setUTCFullYear","setUTCHours","setUTCMilliseconds","setUTCMinutes","setUTCMonth","setUTCSeconds","setYear","shiftKey","startsWith","String","stopImmediatePropagation","stopPropagation","SQRT1_2","SQRT2","strictErrorChecking","storageArea","tabIndex","tagName","targetTouches","textContent","timeEnd(","timeStamp","toDateString","toGMTString","toExponential","toFixed","toJSON","toISOString","toLocaleDateString","toLocaleLowerCase","toLocaleString","toLocaleTimeString","toLocaleUpperCase","toLowerCase","toPrecision","toString","toTimeString","toUpperCase","toUTCString","URL","userAgent","UTC","valueOf","watchPosition"];
-
-						var split = name.split(/(?=[A-Z])/);
-						var split_az = name.split(/(?=[a-z])/);
+						const name = node.name.replace(new RegExp(allowed_uppercases.join('|'), 'i'), (match) => match.toLowerCase());
 
 						// detect camelCase
-						if ((split.length > 1 && split_az.length > 1) && !array_system_var.includes(name) && !whitelist.includes(name)) {
+						if (name.match(general_camelcase_pattern)) {
+							// ignoring system builtins and whitelisted identifiers
+							if (array_system_var.includes(name) || whitelist.includes(name)) {
+								return true;
+							}
+
+							if (allow_uppercamel && name.match(uppercamelcase_pattern)) {
+								return true;
+							}
+
+							if (allowed_patterns.length > 0 && name.match(allowed_pattern)) {
+								return true;
+							}
+
 							if (filter(node)) {
 								return true;
 							}
 
+							const variables = context.getDeclaredVariables(node.parent);
+							const variable = variables.length > 0 ? variables[0]: {};
+							const references = variable.references || [];
+							const reference_nodes = references.map((reference) => reference.identifier);
+
 							// error message on ide
 							context.report({
-								message: `Identifiers must be snake_case: {{ identifier }} (${node.parent.type})`,
+								message: `Identifiers must be snake_case: {{ identifier }} (${node.parent.type}) (refs: ${references.length})`,
 								node: node,
 								data: {
+                  // Returning back the original name
 									identifier: node.name,
 								},
 
 								// --fix eslint parameter: detect camelCase and convert in snake_case
 								fix (fixer) {
-									return fixer.replaceText(node, node.name.split(/(?=[A-Z])/).join("_").toLowerCase());
+									const nodes = new Set([].concat([node], reference_nodes));
+									return [...nodes].map((node) => (
+										fixer.replaceText(node, whitelist_uppercase(allowed_uppercases, to_snakecase(node.name)))
+									));
 								}
 							});
 						}
